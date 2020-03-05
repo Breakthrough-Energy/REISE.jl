eb09d1455cf9e589aeb6c8a4e36f8291ffe5cca8
Daniel Olsen <dolsen@intven.com>
2020-03-05 09:57:50 -0800

    refactor: split code into: types, read, prepare, model, query, save

---
+++ src/query.jl
@@ -0,0 +1,105 @@
+"""
+    get_results(model)
+
+Extract the results of a simulation, store in a struct.
+"""
+function get_results(m::JuMP.Model)::Results
+    status = JuMP.termination_status(m)
+    # These variables will always be in the results
+    pg = get_2d_variable_values(m, "pg")
+    pf = get_2d_variable_values(m, "pf")
+    lmp = -1 * get_2d_constraint_duals(m, "powerbalance")
+    congl = -1 * get_2d_constraint_duals(m, "branch_min")
+    congu = -1 * get_2d_constraint_duals(m, "branch_max")
+    f = JuMP.objective_value(m)
+    # These variables will only be in the results if the model has storage
+    # Initialize with empty arrays, to be discarded later if they stay empty
+    storage_pg = zeros(0, 0)
+    storage_e = zeros(0, 0)
+    try
+        storage_dis = get_2d_variable_values(m, "storage_dis")
+        storage_chg = get_2d_variable_values(m, "storage_chg")
+        storage_pg = storage_dis - storage_chg
+        storage_e = get_2d_variable_values(m, "storage_soc")
+    catch e
+        if isa(e, BoundsError)
+            # Thrown by get_2d_variable_values, variable does not exist
+        else
+            # Unknown error, rethrow it
+            rethrow(e)
+        end
+    end
+    
+    results = Results(;
+        pg=pg, pf=pf, lmp=lmp, congl=congl, congu=congu, f=f,
+        storage_pg=storage_pg, storage_e=storage_e, status=status)
+    return results
+end
+
+
+"""
+    get_2d_variable_values(m, "pg")
+
+Get the values of the variables whose names begin with a given string.
+Returns a 2d array of values, shape inferred from the last variable's name.
+"""
+function get_2d_variable_values(m::JuMP.Model, s::String)::Array{Float64,2}
+    function stringmatch(s::String, v::JuMP.VariableRef)
+        occursin(s * "[", JuMP.name(v))
+    end
+    vars = JuMP.all_variables(m)
+    match_idxs = stringmatch.(s, vars)::BitArray{1}
+    match_vars = vars[match_idxs]::Array{JuMP.VariableRef,1}
+    # What do the indices of the first, second, and last look like?
+    regex_str = r"\[(\d+),(\d+)\]"
+    first_dim_strs = match(regex_str, JuMP.name(match_vars[1])).captures
+    first_dims = Int64[parse(Int,s) for s in first_dim_strs]
+    second_dim_strs = match(regex_str, JuMP.name(match_vars[2])).captures
+    second_dims = Int64[parse(Int,s) for s in second_dim_strs]
+    end_dim_strs = match(regex_str, JuMP.name(match_vars[end])).captures
+    end_dims = Int64[parse(Int,s) for s in end_dim_strs]
+    # Use our knowledge of the dims to appropriately reshape the outputs
+    match_vars = JuMP.value.(match_vars)
+    if (second_dims - first_dims) == [0, 1]
+        match_vars = transpose(reshape(match_vars, (end_dims[2], end_dims[1])))
+    else
+        match_vars = reshape(match_vars, tuple(end_dims...))
+    end
+    return match_vars
+end
+
+
+"""
+    get_2d_constraint_duals(m, "powerbalance")
+
+Get the duals of the constraints whose names begin with a given string.
+Returns a 2d array of values, shape inferred from the last constraints's name.
+"""
+function get_2d_constraint_duals(m::JuMP.Model, s::String)::Array{Float64,2}
+    function stringmatch(s::String, v::JuMP.ConstraintRef)
+        occursin(s, JuMP.name(v))
+    end
+    lt = JuMP.all_constraints(m, JuMP.AffExpr, JuMP.MOI.LessThan{Float64})
+    eq = JuMP.all_constraints(m, JuMP.AffExpr, JuMP.MOI.EqualTo{Float64})
+    gt = JuMP.all_constraints(m, JuMP.AffExpr, JuMP.MOI.GreaterThan{Float64})
+    cons = [lt; eq; gt]
+    match_idxs = stringmatch.(s, cons)::BitArray{1}
+    match_cons = cons[match_idxs]::Array{
+        JuMP.ConstraintRef{JuMP.Model,_A,JuMP.ScalarShape} where _A,1}
+    # What do the indices of the first, second, and last look like?
+    regex_str = r"\[(\d+),(\d+)\]"
+    first_dim_strs = match(regex_str, JuMP.name(match_cons[1])).captures
+    first_dims = Int64[parse(Int,s) for s in first_dim_strs]
+    second_dim_strs = match(regex_str, JuMP.name(match_cons[2])).captures
+    second_dims = Int64[parse(Int,s) for s in second_dim_strs]
+    end_dim_strs = match(regex_str, JuMP.name(match_cons[end])).captures
+    end_dims = Int64[parse(Int,s) for s in end_dim_strs]
+    # Use our knowledge of the dims to appropriately reshape the outputs
+    match_cons = JuMP.shadow_price.(match_cons)
+    if (second_dims - first_dims) == [0, 1]
+        match_cons = transpose(reshape(match_cons, (end_dims[2], :)))
+    else
+        match_cons = reshape(match_cons, (:, end_dims[2]))
+    end
+    return match_cons
+end
---
+++ src/types.jl
@@ -0,0 +1,54 @@
+Base.@kwdef struct Case
+    # We create a struct to hold case data in a type-declared format
+    # `Base.@kwdef` allows us to instantiate this via keywords
+
+    branchid::Array{Int64,1}
+    branch_from::Array{Int64,1}
+    branch_to::Array{Int64,1}
+    branch_reactance::Array{Float64,1}
+    branch_rating::Array{Float64,1}
+
+    dclineid::Array{Int64,1}
+    dcline_from::Array{Int64,1}
+    dcline_to::Array{Int64,1}
+    dcline_rating::Array{Float64,1}
+
+    busid::Array{Int64,1}
+    bus_demand::Array{Float64,1}
+    bus_zone::Array{Int64,1}
+
+    genid::Array{Int64,1}
+    genfuel::Array{String,1}
+    gen_bus::Array{Int64,1}
+    gen_pmax::Array{Float64,1}
+    gen_pmin::Array{Float64,1}
+    gen_ramp30::Array{Float64,1}
+
+    gencost::Array{Float64,2}
+    gencost_orig::Array{Float64,2}
+
+    demand::DataFrames.DataFrame
+    hydro::DataFrames.DataFrame
+    wind::DataFrames.DataFrame
+    solar::DataFrames.DataFrame
+end
+
+
+Base.@kwdef struct Storage
+    gen::Array{Float64,2}
+    sd_table::DataFrames.DataFrame
+end
+
+
+Base.@kwdef struct Results
+    # We create a struct to hold case results in a type-declared format
+    pg::Array{Float64,2}
+    pf::Array{Float64,2}
+    lmp::Array{Float64,2}
+    congu::Array{Float64,2}
+    congl::Array{Float64,2}
+    storage_pg::Array{Float64,2}
+    storage_e::Array{Float64,2}
+    f::Float64
+    status::JuMP.MOI.TerminationStatusCode
+end
