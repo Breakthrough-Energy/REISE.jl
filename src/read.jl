232b4920265f1f34c3b48c406c32f5f78e3f8243
danielolsen <danielolsen@users.noreply.github.com>
2020-03-31 16:40:15 -0700

    Fix dcline load bug (#28)

--- src/read.jl
+++ src/read.jl
@@ -19,8 +19,12 @@ function read_case(filepath)
     case["branch_rating"] = mpc["branch"][:,6]
 
     # DC branches
-    if MAT.exists(case_mat_file, "dcline")
-        case["dclineid"] = dropdims(mpc["dclineid"], dims=2)
+    if "dcline" in keys(mpc)
+        if isa(mpc["dclineid"], Int)
+            case["dclineid"] = Int64[mpc["dclineid"]]
+        else
+            case["dclineid"] = dropdims(mpc["dclineid"], dims=2)
+        end
         case["dcline_from"] = convert(Array{Int,1}, mpc["dcline"][:,1])
         case["dcline_to"] = convert(Array{Int,1}, mpc["dcline"][:,2])
         case["dcline_rating"] = mpc["dcline"][:,11]
