439179b9bda21ea3c655b4ab2ad288cc11d39b97
Daniel Olsen <dolsen@intven.com>
2020-05-18 21:56:12 -0700

    refactor: add function to make Sets struct

--- src/model.jl
+++ src/model.jl
@@ -84,8 +84,75 @@ function _build_segment_slope(case::Case, segment_idx, segment_width)::Matrix
 end
 
 
+function _make_sets(case::Case)::Sets
+    _make_sets(case, nothing)
+end
+
+
+function _make_sets(case::Case, storage::Union{Storage,Nothing})::Sets
+    # Positional indices from mpc.gencost
+    MODEL = 1
+    NCOST = 4
+    # Buses
+    num_bus = length(case.busid)
+    bus_idx = 1:num_bus
+    bus_id2idx = Dict(case.busid .=> bus_idx)
+    load_bus_idx = findall(case.bus_demand .> 0)
+    num_load_bus = length(load_bus_idx)
+    # Sets - branches
+    branch_rating = vcat(case.branch_rating, case.dcline_rating)
+    branch_rating[branch_rating .== 0] .= Inf
+    num_branch_ac = length(case.branchid)
+    num_branch = num_branch_ac + length(case.dclineid)
+    branch_idx = 1:num_branch
+    noninf_branch_idx = findall(branch_rating .!= Inf)
+    all_branch_to = vcat(case.branch_to, case.dcline_to)
+    all_branch_from = vcat(case.branch_from, case.dcline_from)
+    branch_to_idx = Int64[bus_id2idx[b] for b in all_branch_to]
+    branch_from_idx = Int64[bus_id2idx[b] for b in all_branch_from]
+    # Sets - generators
+    num_gen = length(case.genid)
+    gen_idx = 1:num_gen
+    # Subsets - generators
+    gen_wind_idx = gen_idx[findall(
+        (case.genfuel .== "wind") .| (case.genfuel .== "wind_offshore"))]
+    gen_solar_idx = gen_idx[findall(case.genfuel .== "solar")]
+    gen_hydro_idx = gen_idx[findall(case.genfuel .== "hydro")]
+    renewable_idx = sort(vcat(gen_wind_idx, gen_solar_idx, gen_hydro_idx))
+    case.gen_pmax[renewable_idx] .= Inf
+    noninf_pmax = findall(case.gen_pmax .!= Inf)
+    num_wind = length(gen_wind_idx)
+    num_solar = length(gen_solar_idx)
+    num_hydro = length(gen_hydro_idx)
+    # Generator cost curve segments
+    piecewise_enabled = (sum(case.gencost[:, MODEL] .== 1) > 0)
+    @assert(piecewise_enabled, "No piecewise segments detected. "
+                               * "Did you forget to linearize_gencost?")
+    num_segments = convert(Int, maximum(case.gencost[:, NCOST])) - 1
+    segment_idx = 1:num_segments
+    # Storage
+    storage_enabled = isa(storage, Storage) && (size(storage.gen, 1) > 0)
+    num_storage = storage_enabled ? size(storage.gen, 1) : 0
+    storage_idx = storage_enabled ? (1:num_storage) : nothing
+
+    sets = Sets(;
+        num_bus=num_bus, bus_idx=bus_idx, bus_id2idx=bus_id2idx,
+        load_bus_idx=load_bus_idx, num_load_bus=num_load_bus,
+        num_branch=num_branch, num_branch_ac=num_branch_ac,
+        branch_idx=branch_idx, noninf_branch_idx=noninf_branch_idx,
+        branch_to_idx=branch_to_idx, branch_from_idx=branch_from_idx,
+        num_gen=num_gen, gen_idx=gen_idx, noninf_pmax=noninf_pmax,
+        gen_hydro_idx=gen_hydro_idx, gen_solar_idx=gen_solar_idx,
+        gen_wind_idx=gen_wind_idx, renewable_idx=renewable_idx,
+        num_wind=num_wind, num_solar=num_solar, num_hydro=num_hydro,
+        num_segments=num_segments, segment_idx=segment_idx,
+        num_storage=num_storage, storage_idx=storage_idx)
+end
+
+
 """
-    _build_model(case=case, start_index=x, interval_length=y[, kwargs...])
+    _build_model(m; case=case, storage=storage, start_index=x,
+                 interval_length=y[, kwargs...])
 
 Given a Case object and a set of options, build an optimization model.
 Returns a JuMP.Model instance.
@@ -102,10 +169,6 @@ function _build_model(m::JuMP.Model; case::Case, storage::Storage,
                      storage_e0::Array{Float64,1}=Float64[])::Tuple{
                         JuMP.Model, VariablesOfInterest}
     # Positional indices from mpc.gencost
-    MODEL = 1
-    STARTUP = 2
-    SHUTDOWN = 3
-    NCOST = 4
     COST = 5
     # Positional indices from mpc.gen
     PMAX = 9
@@ -116,103 +179,69 @@ function _build_model(m::JuMP.Model; case::Case, storage::Storage,
     num_hour = interval_length
     hour_idx = 1:interval_length
     end_index = start_index + interval_length - 1
-    # Sets - buses
-    num_bus = length(case.busid)
-    bus_idx = 1:num_bus
-    bus_id2idx = Dict(case.busid .=> bus_idx)
-    load_bus_idx = findall(case.bus_demand .> 0)
-    num_load_bus = length(load_bus_idx)
-    load_bus_map = sparse(
-        load_bus_idx, 1:num_load_bus, 1, num_bus, num_load_bus)
-    # Sets - branches
-    branch_rating = vcat(case.branch_rating, case.dcline_rating)
-    branch_rating[branch_rating .== 0] .= Inf
-    num_branch_ac = length(case.branchid)
-    num_branch = num_branch_ac + length(case.dclineid)
-    branch_idx = 1:num_branch
-    noninf_branch_idx = findall(branch_rating .!= Inf)
-    # Sets - generators
-    num_gen = length(case.genid)
-    gen_idx = 1:num_gen
-    # Sets/parameters - generator cost curve segments
-    piecewise_enabled = (sum(case.gencost[:, MODEL] .== 1) > 0)
-    @assert(piecewise_enabled, "No piecewise segments detected. "
-                               * "Did you forget to linearize_gencost?")
-    # For now, assume all gens are represented with same number of segments
-    num_segments = convert(Int, maximum(case.gencost[:, NCOST])) - 1
-    segment_width = (case.gen_pmax - case.gen_pmin) ./ num_segments
-    segment_idx = 1:num_segments
-    fixed_cost = case.gencost[:, COST+1]
-    segment_slope = _build_segment_slope(case, segment_idx, segment_width)
-    # Subsets - generators
-    gen_wind_idx = gen_idx[findall(
-        (case.genfuel .== "wind") .| (case.genfuel .== "wind_offshore"))]
-    gen_solar_idx = gen_idx[findall(case.genfuel .== "solar")]
-    gen_hydro_idx = gen_idx[findall(case.genfuel .== "hydro")]
-    renewable_idx = sort(vcat(gen_wind_idx, gen_solar_idx, gen_hydro_idx))
-    case.gen_pmax[renewable_idx] .= Inf
-    noninf_pmax = findall(case.gen_pmax .!= Inf)
-    num_wind = length(gen_wind_idx)
-    num_solar = length(gen_solar_idx)
-    num_hydro = length(gen_hydro_idx)
-    # Sets/parameters - storage (if present)
-    storage_enabled = (size(storage.gen, 1) > 0)
-    if storage_enabled
-        num_storage = size(storage.gen, 1)
-        storage_idx = 1:num_storage
-        storage_max_dis = storage.gen[:, PMAX]
-        storage_max_chg = -1 * storage.gen[:, PMIN]
-        storage_min_energy = storage.sd_table.MinStorageLevel
-        storage_max_energy = storage.sd_table.MaxStorageLevel
-        storage_bus_idx = [bus_id2idx[b] for b in storage.gen[:, 1]]
-        storage_map = sparse(storage_bus_idx, storage_idx, 1, num_bus,
-                             num_storage)::SparseMatrixCSC
-    end
+    # Sets - static
+    sets = _make_sets(case, storage)
 
     println("parameters: ", Dates.now())
     # Parameters
+    # Load bus mapping
+    load_bus_map = sparse(sets.load_bus_idx, 1:sets.num_load_bus, 1,
+                          sets.num_bus, sets.num_load_bus)
     # Generator topology matrix
     gen_map = _make_gen_map(case)
+    # Generation segments
+    segment_width = (case.gen_pmax - case.gen_pmin) ./ sets.num_segments
+    fixed_cost = case.gencost[:, COST+1]
+    segment_slope = _build_segment_slope(case, sets.segment_idx, segment_width)
     # Branch connectivity matrix
-    all_branch_to = vcat(case.branch_to, case.dcline_to)
-    all_branch_from = vcat(case.branch_from, case.dcline_from)
-    branch_to_idx = Int64[bus_id2idx[b] for b in all_branch_to]
-    branch_from_idx = Int64[bus_id2idx[b] for b in all_branch_from]
     branch_map = _make_branch_map(case)
+    branch_rating = vcat(case.branch_rating, case.dcline_rating)
     # Demand by bus
     bus_demand = _make_bus_demand(case, start_index, end_index)
     bus_demand *= demand_scaling
     simulation_hydro = Matrix(case.hydro[start_index:end_index, 2:end])
     simulation_solar = Matrix(case.solar[start_index:end_index, 2:end])
     simulation_wind = Matrix(case.wind[start_index:end_index, 2:end])
+    # Storage parameters (if present)
+    storage_enabled = (sets.num_storage > 0)
+    if storage_enabled
+        storage_max_dis = storage.gen[:, PMAX]
+        storage_max_chg = -1 * storage.gen[:, PMIN]
+        storage_min_energy = storage.sd_table.MinStorageLevel
+        storage_max_energy = storage.sd_table.MaxStorageLevel
+        storage_bus_idx = [sets.bus_id2idx[b] for b in storage.gen[:, 1]]
+        storage_map = sparse(storage_bus_idx, sets.storage_idx, 1,
+                             sets.num_bus, sets.num_storage)::SparseMatrixCSC
+    end
 
     println("variables: ", Dates.now())
     # Variables
     # Explicitly declare containers as Array of VariableRefs, not DenseArrayAxis
     JuMP.@variables(m, begin
-        pg[gen_idx, hour_idx] >= 0, (container=Array)
-        pg_seg[gen_idx, segment_idx, hour_idx] >= 0, (container=Array)
-        pf[branch_idx, hour_idx], (container=Array)
-        theta[bus_idx, hour_idx], (container=Array)
+        pg[sets.gen_idx, hour_idx] >= 0, (container=Array)
+        pg_seg[sets.gen_idx, sets.segment_idx, hour_idx] >= 0, (container=Array)
+        pf[sets.branch_idx, hour_idx], (container=Array)
+        theta[sets.bus_idx, hour_idx], (container=Array)
     end)
     if load_shed_enabled
         JuMP.@variable(m,
-            0 <= load_shed[i in 1:num_load_bus, j in hour_idx]
-            <= bus_demand[load_bus_idx[i], j],
+            0 <= load_shed[i in 1:sets.num_load_bus, j in hour_idx]
+            <= bus_demand[sets.load_bus_idx[i], j],
             container=Array)
     end
     if trans_viol_enabled
         JuMP.@variable(m,
-            0 <= trans_viol[i in branch_idx, j in hour_idx], container=Array)
+            0 <= trans_viol[i in sets.branch_idx, j in hour_idx],
+            container=Array)
     end
     if storage_enabled
         JuMP.@variables(m, begin
-            (0 <= storage_chg[i in storage_idx, j in hour_idx]
+            (0 <= storage_chg[i in sets.storage_idx, j in hour_idx]
                 <= storage_max_chg[i]), (container=Array)
-            (0 <= storage_dis[i in storage_idx, j in hour_idx]
+            (0 <= storage_dis[i in sets.storage_idx, j in hour_idx]
                 <= storage_max_dis[i]), (container=Array)
             (storage_min_energy[i]
-                <= storage_soc[i in storage_idx, j in hour_idx]
+                <= storage_soc[i in sets.storage_idx, j in hour_idx]
                 <= storage_max_energy[i]), (container=Array)
         end)
     end
@@ -234,7 +263,7 @@ function _build_model(m::JuMP.Model; case::Case, storage::Storage,
     end
     JuMP.@constraint(m, powerbalance, (injections .== withdrawls))
     println("powerbalance, setting names: ", Dates.now())
-    for i in bus_idx, j in hour_idx
+    for i in sets.bus_idx, j in hour_idx
         JuMP.set_name(powerbalance[i, j],
                       "powerbalance[" * string(i) * "," * string(j) * "]")
     end
@@ -242,7 +271,7 @@ function _build_model(m::JuMP.Model; case::Case, storage::Storage,
     if storage_enabled
         println("storage soc_tracking: ", Dates.now())
         JuMP.@constraint(m,
-            soc_tracking[i in storage_idx, h in 1:(num_hour-1)],
+            soc_tracking[i in sets.storage_idx, h in 1:(num_hour-1)],
             storage_soc[i, h+1] == (
                 storage_soc[i, h]
                 + storage.sd_table.InEff[i] * storage_chg[i, h+1]
@@ -250,7 +279,7 @@ function _build_model(m::JuMP.Model; case::Case, storage::Storage,
             container=Array)
         println("storage initial_soc: ", Dates.now())
         JuMP.@constraint(m,
-            initial_soc[i in storage_idx],
+            initial_soc[i in sets.storage_idx],
             storage_soc[i, 1] == (
                 storage_e0[i]
                 + storage.sd_table.InEff[i] * storage_chg[i, 1]
@@ -282,11 +311,12 @@ function _build_model(m::JuMP.Model; case::Case, storage::Storage,
 
     println("segment_max: ", Dates.now())
     JuMP.@constraint(m,
-        segment_max[i in noninf_pmax, s in segment_idx, h in hour_idx],
+        segment_max[
+            i in sets.noninf_pmax, s in sets.segment_idx, h in hour_idx],
         pg_seg[i, s, h] <= segment_width[i])
     println("segment_add: ", Dates.now())
     JuMP.@constraint(m,
-        segment_add[i in noninf_pmax, h in hour_idx],
+        segment_add[i in sets.noninf_pmax, h in hour_idx],
         pg[i, h] == case.gen_pmin[i] + sum(pg_seg[i, :, h]))
 
     if trans_viol_enabled
@@ -294,42 +324,45 @@ function _build_model(m::JuMP.Model; case::Case, storage::Storage,
             branch_limit, branch_rating + trans_viol)
     else
         JuMP.@expression(m,
-            branch_limit[br in branch_idx, h in hour_idx], branch_rating[br])
+            branch_limit[br in sets.branch_idx, h in hour_idx],
+            branch_rating[br])
     end
     println("branch_min, branch_max: ", Dates.now())
     JuMP.@constraint(m,
-        branch_min[br in noninf_branch_idx, h in hour_idx],
+        branch_min[br in sets.noninf_branch_idx, h in hour_idx],
         -1 * branch_limit[br, h] <= pf[br, h])
     println("branch_max: ", Dates.now())
     JuMP.@constraint(m,
-        branch_max[br in noninf_branch_idx, h in hour_idx],
+        branch_max[br in sets.noninf_branch_idx, h in hour_idx],
         pf[br, h] <= branch_limit[br, h])
 
     println("branch_angle: ", Dates.now())
     # Explicit numbering here so that we constrain AC branches but not DC
     JuMP.@constraint(m,
-        branch_angle[br in 1:num_branch_ac, h in hour_idx],
+        branch_angle[br in 1:sets.num_branch_ac, h in hour_idx],
         (case.branch_reactance[br] * pf[br, h]
-            == (theta[branch_to_idx[br], h] - theta[branch_from_idx[br], h])))
+            == (theta[sets.branch_to_idx[br], h]
+                - theta[sets.branch_from_idx[br], h])))
 
     # Constrain variable generators based on profiles
     println("hydro_fixed: ", Dates.now())
     JuMP.@constraint(m,
-        hydro_fixed[i in 1:num_hydro, h in hour_idx],
-        pg[gen_hydro_idx[i], h] == simulation_hydro[h, i])
+        hydro_fixed[i in 1:sets.num_hydro, h in hour_idx],
+        pg[sets.gen_hydro_idx[i], h] == simulation_hydro[h, i])
     println("solar_max: ", Dates.now())
     JuMP.@constraint(m,
-        solar_max[i in 1:num_solar, h in hour_idx],
-        pg[gen_solar_idx[i], h] <= simulation_solar[h, i])
+        solar_max[i in 1:sets.num_solar, h in hour_idx],
+        pg[sets.gen_solar_idx[i], h] <= simulation_solar[h, i])
     println("wind_max: ", Dates.now())
     JuMP.@constraint(m,
-        wind_max[i in 1:num_wind, h in hour_idx],
-        pg[gen_wind_idx[i], h] <= simulation_wind[h, i])
+        wind_max[i in 1:sets.num_wind, h in hour_idx],
+        pg[sets.gen_wind_idx[i], h] <= simulation_wind[h, i])
 
     println("objective: ", Dates.now())
     # Start with generator variable O & M, piecewise
     obj = JuMP.@expression(m,
-        sum(segment_slope[noninf_pmax, :] .* pg_seg[noninf_pmax, :, :]))
+        sum(segment_slope[sets.noninf_pmax, :]
+            .* pg_seg[sets.noninf_pmax, :, :]))
     # Add fixed costs
     JuMP.add_to_expression!(
         obj, JuMP.@expression(m, num_hour * sum(fixed_cost)))
