7691903fbceccb27cbf5de090faa7c0bf2b1f948
Jon Hagg <jon.hagg@breakthroughenergy.org>
2021-05-10 12:32:36 -0700

    fix: use os path join

--- pyreisejl/utility/const.py
+++ pyreisejl/utility/const.py
@@ -1,9 +1,9 @@
-import posixpath
+import os
 
 DATA_ROOT_DIR = "/mnt/bes/pcm"
 
-SCENARIO_LIST = posixpath.join(DATA_ROOT_DIR, "ScenarioList.csv")
-EXECUTE_LIST = posixpath.join(DATA_ROOT_DIR, "ExecuteList.csv")
-EXECUTE_DIR = posixpath.join(DATA_ROOT_DIR, "tmp")
-INPUT_DIR = posixpath.join(DATA_ROOT_DIR, "data", "input")
-OUTPUT_DIR = posixpath.join(DATA_ROOT_DIR, "data", "output")
+SCENARIO_LIST = os.path.join(DATA_ROOT_DIR, "ScenarioList.csv")
+EXECUTE_LIST = os.path.join(DATA_ROOT_DIR, "ExecuteList.csv")
+EXECUTE_DIR = os.path.join(DATA_ROOT_DIR, "tmp")
+INPUT_DIR = os.path.join(DATA_ROOT_DIR, "data", "input")
+OUTPUT_DIR = os.path.join(DATA_ROOT_DIR, "data", "output")
