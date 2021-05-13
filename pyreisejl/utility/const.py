a258dd51eb31bb50572b2ce6b705b3e83ebc8027
Jon Hagg <jon.hagg@breakthroughenergy.org>
2021-05-13 12:50:49 -0700

    fix: set root dir based on deployment mode

--- pyreisejl/utility/const.py
+++ pyreisejl/utility/const.py
@@ -1,6 +1,10 @@
 import os
+from pathlib import Path
 
-DATA_ROOT_DIR = "/mnt/bes/pcm"
+if os.getenv("DEPLOYMENT_MODE") is not None:
+    DATA_ROOT_DIR = os.path.join(Path.home(), "ScenarioData", "")
+else:
+    DATA_ROOT_DIR = "/mnt/bes/pcm"
 
 SCENARIO_LIST = os.path.join(DATA_ROOT_DIR, "ScenarioList.csv")
 EXECUTE_LIST = os.path.join(DATA_ROOT_DIR, "ExecuteList.csv")
