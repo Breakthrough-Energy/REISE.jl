2a73790deb0a4832871dce494d27c9ca10e92427
Daniel Olsen <daniel@breakthroughenergy.org>
2021-07-26 13:09:52 -0700

    refactor: improve error messaging for Gurobi environment issues

--- src/solver_specific/gurobi.jl
+++ src/solver_specific/gurobi.jl
@@ -6,12 +6,17 @@ function run_scenario_gurobi(; solver_kwargs::Union{Dict, Nothing}=nothing, kwar
     solver_kwargs = something(solver_kwargs, Dict("Method" => 2, "Crossover" => 0))
     try
         global env = Gurobi.Env()
+    catch e
+        println("Error encountered starting Gurobi.")
+        throw(e)
+    end
+    try
         global m = run_scenario(;
             optimizer_factory=env, solver_kwargs=solver_kwargs, kwargs...)
     finally
         Gurobi.finalize(JuMP.backend(m))
         Gurobi.finalize(env)
-        println("Connection closed successfully!")
+        println("Connection to Gurobi closed successfully!")
     end
     # Return `nothing` to prevent `m` from the `try` block from being returned
     return nothing
