9cd3986dcc311237b8362adf0a5fec461216ab90
danielolsen <danielolsen@users.noreply.github.com>
2022-03-10 17:45:08 -0800

    fix: avoid error when demand flexibility isn't enabled (#169)

--- src/REISE.jl
+++ src/REISE.jl
@@ -70,7 +70,11 @@ function run_scenario(;
     println("All scenario files loaded!")
     case = reise_data_mods(case; num_segments=num_segments)
     sets = _make_sets(case)
-    demand_flexibility = reformat_demand_flexibility_input(case, demand_flexibility, sets)
+    if demand_flexibility.enabled
+        demand_flexibility = reformat_demand_flexibility_input(
+            case, demand_flexibility, sets
+        )
+    end
     save_input_mat(case, storage, inputfolder, outputfolder)
     # Create final model kwargs by merging defaults with user-specified
     default_model_kwargs = Dict(
