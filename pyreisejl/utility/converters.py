3400a070c8512db7a4331a75f718b2a3be61c923
Jen Hagg <jen@breakthroughenergy.org>
2023-02-01 16:58:11 -0800

    refactor: construct column lists using str.split

--- pyreisejl/utility/converters.py
+++ pyreisejl/utility/converters.py
@@ -3,26 +3,14 @@ import os
 import pickle
 
 cols = {
-    "branch": ["branch_id", "from_bus_id", "to_bus_id", "x", "rateA"],
-    "dcline": ["dcline_id", "from_bus_id", "to_bus_id", "Pmin", "Pmax"],
-    "bus": ["bus_id", "Pd", "zone_id"],
-    "plant": [
-        "plant_id",
-        "bus_id",
-        "status",
-        "Pmin",
-        "Pmax",
-        "type",
-        "ramp_30",
-        "GenFuelCost",
-        "GenIOB",
-        "GenIOC",
-        "GenIOD",
-    ],
-    "storage_gen": ["bus_id", "Pmin", "Pmax"],
+    "branch": "branch_id from_bus_id to_bus_id x rateA".split(),
+    "dcline": "dcline_id from_bus_id to_bus_id Pmin Pmax".split(),
+    "bus": "bus_id Pd zone_id".split(),
+    "plant": "plant_id bus_id status Pmin Pmax type ramp_30 GenFuelCost GenIOB GenIOC GenIOD".split(),
+    "storage_gen": "bus_id Pmin Pmax".split(),
 }
 
-drop_cols = {"gencost_before": ["plant_id", "interconnect"]}
+drop_cols = {"gencost_before": "plant_id interconnect".split()}
 drop_cols["gencost_after"] = drop_cols["gencost_before"]
 
 
